079d73eaf974db1a5b4e71439ba557de1f915a9e
Ryan L <idealizinc@gmail.com>
2022-02-11 16:01:41 +0800

    Enable setting for debug

--- src/ImageRecognitionServer.py
+++ src/ImageRecognitionServer.py
@@ -19,12 +19,16 @@ RECEIVER_FILE_PATH = RECEIVER_PATH + 'out.jpg'
 ANNOTATION_CLASSES = ['1_blue', '2_green', '3_red', '4_white', '5_yellow', '6_blue', '7_green', '8_red', '9_white', 'a_red', 'b_green', 'bullseye', 'c_white', 'circle_yellow', 'd_blue', 'down_arrow_red', 'e_yellow', 'f_red', 'g_green', 'h_white', 'left_arrow_blue', 'right_arrow_green', 's_blue', 't_yellow', 'u_red', 'up_arrow_white', 'v_green', 'w_white', 'x_blue', 'y_yellow', 'z_red']
 NUM_CLASSES = 31
 
+# DEBUG Parameters
+DEBUG_MODE_ON = True
+
 # Main runtime
 def main():
-    sr = SymRec(WEIGHT_PATH, ANNOTATION_CLASSES, NUM_CLASSES)
-    msg = sr.ProcessSourceImages(IMAGE_PATH, '../inferences/', True)
-    print("Detected: " + msg)
-    #IRS_Server()
+    if DEBUG_MODE_ON:
+        sr = SymRec(WEIGHT_PATH, ANNOTATION_CLASSES, NUM_CLASSES)
+        msg = sr.ProcessSourceImages(IMAGE_PATH, '../inferences/', True)
+        print("Detected: " + msg)
+    else: IRS_Server()
 
 def IRS_Server():
     # Connect to RPi
