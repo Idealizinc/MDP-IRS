60330148760af96ffa69f8c044422bf61dd48130
Ryan L <idealizinc@gmail.com>
2022-02-11 09:22:24 +0800

    Setting Output Detected Symbol Name
    Minor testing of area calculations
    - Potentially bugged

--- src/ImageRecognitionServer.py
+++ src/ImageRecognitionServer.py
@@ -11,9 +11,14 @@ WEIGHT_PATH = "../weights/e40b16v8best.pt"
 YOLO_PATH = "../yolov5"
 IMAGE_PATH = "../img/1_5.jpg"
 
+# Other Constants
+ANNOTATION_CLASSES = ['1_blue', '2_green', '3_red', '4_white', '5_yellow', '6_blue', '7_green', '8_red', '9_white', 'a_red', 'b_green', 'bullseye', 'c_white', 'circle_yellow', 'd_blue', 'down_arrow_red', 'e_yellow', 'f_red', 'g_green', 'h_white', 'left_arrow_blue', 'right_arrow_green', 's_blue', 't_yellow', 'u_red', 'up_arrow_white', 'v_green', 'w_white', 'x_blue', 'y_yellow', 'z_red']
+NUM_CLASSES = 31
+
 # Main runtime
 def main():
-    sr = SymRec(WEIGHT_PATH)
+    # Get result from processed image
+    sr = SymRec(WEIGHT_PATH, ANNOTATION_CLASSES, NUM_CLASSES)
     msg = sr.ProcessSourceImages(IMAGE_PATH)
-
+    print("Detected: " + msg)
 main()
--- src/SymbolRecognizer.py
+++ src/SymbolRecognizer.py
@@ -10,10 +10,14 @@ import torch # For the inference model
 class SymbolRecognizer:
     ModelStatus = "nil"
     Model = None
+    Classes = None
+    ClassCount = -1
 
     # Class constuctor to setup weights
-    def __init__(self, weightPath):
+    def __init__(self, weightPath, classes, numclasses):
         self.LoadWeights(weightPath)
+        self.Classes = classes
+        self.ClassCount = numclasses
 
     # Import the selected weights file into YOLOv5
     def LoadWeights(self, weightPath):
@@ -22,7 +26,7 @@ class SymbolRecognizer:
         # Initialize the model
         self.Model = torch.hub.load('ultralytics/yolov5', 'custom', path = weightPath)
         # Print status
-        print("YOLOv5 Model initiallized with weight: " + weightPath)
+        print("\nYOLOv5 Model initiallized with weight: " + weightPath)
 
     # Run inference on the source images in the path with the model
     def ProcessSourceImages(self, srcPath):
@@ -31,20 +35,35 @@ class SymbolRecognizer:
         #results.save()  # or .show()
         results.show()
         outputMessage = self.SetupResultString(self.ProcessInferenceResults(results))
-        return 0
+        return outputMessage
 
     # Process results of model inference to determine which symbol is most likely the result
     def ProcessInferenceResults(self, results):
-        print("Processing Inference Results")
+        print("\nProcessing Inference Results")
         #print(results.xyxy[0])  # predictions (tensor)
         print(results.pandas().xyxy[0])  # predictions (pandas)
-        labels, cord = results.xyxyn[0][:, -1].to('cpu').numpy(), results.xyxyn[0][:, :-1].to('cpu').numpy()
-        print(labels) # Yoink labels
-        print(cord) # Coords for each detected label
+        labels, coord = results.xyxyn[0][:, -1].to('cpu').numpy(), results.xyxyn[0][:, :-1].to('cpu').numpy()
+        #print(labels) # Yoink labels
+        #print(coord) # Coords for each detected label
+        # Area calculations
+        if False: # Skipping this section first
+            areaList = []
+            #print(coord)
+            for i in range(len(coord)): # Finding area is bugged
+                y = coord[i][2] - coord[i][1] # Accessing coord values is a problem
+                print(coord[i][2], coord[i][1])
+                x = coord[i][3] - coord[i][0]
+                areaList.append(x * y)
+            print(areaList)
         return labels
 
     # Make use of processed results to create an output string to be sent back to RPi
     def SetupResultString(self, results):
-        print("Setup Result Message")
+        print("\nSetting Up Result Message")
         # Do stuff with passed labels
-        return "test"
+        label = "No Symbol Detected"
+        for idx in results:
+            i = int(idx)
+            if (label[i] != "bullseye"):
+                label = self.Classes[i]
+        return label
